c17c17da8a8135de056d0f418242833b0347f62a
Maxim-Pekov <max.pekov@gmail.com>
2023-02-01 20:38:24 +0300

    Add ModelSerializer.

--- enrollment/views.py
+++ enrollment/views.py
@@ -5,37 +5,31 @@ from .models import Application, Participant
 from rest_framework.serializers import ValidationError
 from rest_framework.serializers import Serializer
 from rest_framework.serializers import CharField
+from rest_framework.serializers import ModelSerializer
 
 
-class ApplicationSerializer(Serializer):
-    contact_phone = CharField()
-    ticket_type = CharField()
+class ApplicationSerializer(ModelSerializer):
+    class Meta:
+        model = Application
+        fields = ['contact_phone', 'ticket_type']
 
-    def validate_ticket_type(self, value):
-        if value not in ['standard-access', 'pro-access', 'premium-access']:
-            raise ValidationError('Wrong value')
-        return value
-
-
-def validate(data):
-    serializer = ApplicationSerializer(data=data)
-    serializer.is_valid(raise_exception=True)
+class ParticipantSerializer(ModelSerializer):
+    class Meta:
+        model = Participant
+        fields = ['first_name', 'last_name', 'email']
 
 
 @api_view(['POST'])
 def enroll(request):
-    validate(request.data)
+    serializer = ApplicationSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
 
     participants = request.data.get('participants', [])  # TODO validate data!
 
-    application = Application.objects.create(
-        contact_phone=str(request.data['contact_phone']),
-        ticket_type=str(request.data['ticket_type']),
-    )
+    if not isinstance(participants, list):
+        raise ValidationError('Expects participants field be a list')
 
-    participants = [Participant(application=application, **fields) for fields in participants]
-    Participant.objects.bulk_create(participants)
+    for fields in participants:
+        serializer = ParticipantSerializer(data=fields)
+        serializer.is_valid(raise_exception=True)  # выкинет ValidationError
 
-    return Response({
-        'application_id': application.id,
-    })
